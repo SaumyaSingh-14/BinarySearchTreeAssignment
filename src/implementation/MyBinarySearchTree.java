6792a2e3e1be86896bfafd32c4e97765d805eda3
Shivamy2 <sy7052789@gmail.com>
2021-04-15 23:49:44 +0530

    Final Commit Modified

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,40 +5,224 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    private implementation.Node<E> root;
 
-    @Override
-    public void insert(E data) {
-
+    public implementation.Node<E> getRoot() {
+        return root;
     }
 
     @Override
-    public boolean search(E searchElement) {
+    public void insert(E data) {
+        implementation.Node<E> node = new implementation.Node<>(data);
+        if (isEmpty()) {
+            root = node;
+        } else {
+            //traverse and reach the position where new node
+            // will be inserted as well as keep reference of parent
+            implementation.Node<E> temp = root;
+            implementation.Node<E> parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (data.compareTo(temp.getData()) <= 0) {
+                    //update temp to refer left subtree
+                    temp = temp.getLeft();
+                } else {
+                    //update temp to refer right subtree
+                    temp = temp.getRight();
+                }
+            }
+            //checking whether new node will be left or right
+            //child of parent
+            if (data.compareTo(parent.getData()) <= 0) {
+                //set new node to left child of parent
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
+    }
+
+    private boolean isEmpty() {
+        if (root == null) {
+            return true;
+        }
         return false;
     }
 
     @Override
-    public void inOrder(Node<E> node) {
-
+    public boolean search(E searchElement) {
+        boolean response = false;
+        //traverse
+        implementation.Node<E> temp = root;
+        while (temp != null) {
+            if (searchElement.compareTo(temp.getData()) == 0) {
+                response = true;
+                break;
+            } else if (searchElement.compareTo(temp.getData()) < 0) {
+                temp = temp.getLeft();
+            } else {
+                temp = temp.getRight();
+            }
+        }
+        return response;
     }
 
     @Override
-    public void preOrder(Node<E> node) {
-
+    public void inOrder(implementation.Node<E> node) {
+        if (node != null) {
+            inOrder(node.getLeft());
+            System.out.print(node.getData() + ", ");
+            inOrder(node.getRight());
+        }
     }
 
     @Override
-    public void postOrder(Node<E> node) {
-
+    public void preOrder(implementation.Node<E> node) {
+        if (node != null) {
+            System.out.print(node.getData() + ", ");
+            inOrder(node.getLeft());
+            inOrder(node.getRight());
+        }
     }
 
     @Override
-    public void reverseInOrder(Node<E> node) {
+    public void postOrder(implementation.Node<E> node) {
+        if (node != null) {
+            inOrder(node.getLeft());
+            inOrder(node.getRight());
+            System.out.print(node.getData() + ", ");
+        }
+    }
 
+    @Override
+    public void reverseInOrder(implementation.Node<E> node) {
+        if (node != null) {
+            inOrder(node.getRight());
+            System.out.print(node.getData() + ", ");
+            inOrder(node.getLeft());
+        }
     }
 
     @Override
     public void delete(E data) {
+        implementation.Node<E> temp = root;
+        implementation.Node<E> parent = null;
+        while (temp != null) {
+            if (data.compareTo(temp.getData()) == 0) {
+                break;
+            } else {
+                parent = temp;
+                if (data.compareTo(temp.getData()) < 0) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+        }
+        if(temp != null){
+            //case 1
 
+            if(isLeaf(temp)){
+                //root node
+                if(parent == null){
+                    root = null;
+                }
+                else{
+                    if(data.compareTo(parent.getData()) < 0){
+                        parent.setLeft(null);
+                    }
+                    else{
+                        parent.setRight(null);
+                    }
+                }
+            }
+            //case 2
+            //left child
+            else if(hasLeftChild(temp)){
+                //root node
+                if(parent == null){
+                    root = root.getLeft();
+                }
+                else{
+                    if(data.compareTo(parent.getData()) < 0){
+                        parent.setLeft(temp.getLeft());
+                    }
+                    else{
+                        parent.setRight(temp.getLeft());
+                    }
+                }
+            }
+            //right child
+            else if(hasRightChild(temp)){
+                //root case
+                if(parent == null){
+                    root = root.getRight();
+                }
+                else {
+                    if(data.compareTo(parent.getData()) < 0){
+                        parent.setLeft(temp.getRight());
+                    }
+                    else{
+                        parent.setRight(temp.getRight());
+                    }
+                }
+            }
+            //case 3 two children
+            else{
+                implementation.Node<E> successor = getSuccessor(temp);
+                //delete successor
+                delete(successor.getData());
+                successor.setLeft(temp.getLeft());
+                successor.setRight(temp.getRight());
+                //root case
+                if(parent == null){
+                    root = successor;
+                }
+                else{
+                    if(data.compareTo(parent.getData()) < 0){
+                        parent.setLeft(successor);
+                    }
+                    else{
+                        parent.setRight(successor);
+                    }
+                }
+            }
+            //root node
+        }
+        else{
+            System.out.println("cannot delete");
+        }
+    }
+
+    private implementation.Node<E> getSuccessor(implementation.Node<E> node) {
+        implementation.Node<E> response = null;
+        implementation.Node<E> temp = node.getRight();
+        while (temp.getLeft() != null){
+            temp = temp.getLeft();
+        }
+        response = temp;
+        return response;
+    }
+
+    private boolean hasRightChild(implementation.Node<E> temp) {
+        if(temp.getRight() != null && temp.getLeft() == null){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasLeftChild(implementation.Node<E> temp) {
+        if(temp.getLeft() != null && temp.getRight() == null){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isLeaf(implementation.Node<E> temp) {
+        if(temp.getLeft() == null && temp.getRight() == null){
+            return true;
+        }
+        return false;
     }
 
     @Override
--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,8 +1,21 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
         //create binary search tree
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.insert(54);
+        myBinarySearchTree.insert(8);
+        myBinarySearchTree.insert(14);
+        myBinarySearchTree.insert(98);
+        myBinarySearchTree.insert(34);
+        myBinarySearchTree.insert(65);
         // and perform insert, search, traversal, delete
+        myBinarySearchTree.inOrder(myBinarySearchTree.getRoot());
+        myBinarySearchTree.delete(8);
+        myBinarySearchTree.inOrder(myBinarySearchTree.getRoot());
+        System.out.println(myBinarySearchTree.search(14));
     }
 }
